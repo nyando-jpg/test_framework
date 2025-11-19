9e9fec8671b4b668647195ca429031800fddaa41
nyando-jpg <nyandoratovonjoely@gmail.com>
2025-11-19 06:22:58 +0300

    scan

---
+++ Framework_test/src/test/HelloServlet.java
@@ -0,0 +1,17 @@
+package test;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Bonjour depuis le projet Test !</h1>");
+    }
+}
